1f2d37ff6e53daf7b4e2c4f95162a5a3db4cb785
Anthony Wilkins <anthony@bvhllc.com>
2022-01-13 16:55:14 +0000

    Argument parsing, management features

--- cloudcost.py
+++ cloudcost.py
@@ -5,6 +5,7 @@ from psycopg2 import sql
 
 # Python Standard stuff
 import importlib
+from importlib import util
 import argparse
 import inspect
 from datetime import datetime
@@ -13,6 +14,8 @@ import configparser
 import requests
 from os import path
 import json
+import os
+from pathlib import Path
 
 # XLSX support
 from openpyxl import Workbook
@@ -85,7 +88,8 @@ def create_xlsx():
     return wb, ws, 4
 
 # Queries DB and runs cost against all accounts, all providers
-def run_cost(cur):
+def run_cost(cur, **kwargs):
+    conf = kwargs['conf']
     # Set up our workbook
     wb, ws, i = create_xlsx()
 
@@ -142,10 +146,28 @@ def run_cost(cur):
     fname = "/tmp/cloudcost{}.xlsx".format(datetime.today().strftime("%Y-%m-%d"))
     wb.save(fname)
 
-    # Return the filename so our MM uploader can do its thing
-    return fname
+    # Retry upload 5 times
+    for i in range(5):
+        try:
+            upload_file(fname, **conf['mattermost'])
+        except Exception as err:
+            # Failed but we have retries left
+            if i < 4:
+                # Print and continue
+                print(err)
+                continue
+            else:
+                # Retries exceeded, pass the exception up
+                raise
+        else:
+            # Success, break from retry loop
+            break
 
-def add_account(cur, provider):
+# Add a new account to the DB
+def add_account(cur, **kwargs):
+    args = kwargs['args']
+    provider = args.iaas
+    account = args.account
     # First check if provider module exists
     # It doesn't we can't do anything
     try:
@@ -166,9 +188,23 @@ def add_account(cur, provider):
 
         # do the thing
         cur.execute(create)
+        
+        # Make sure account doesn't already exist
+        query = sql.SQL("select top 1 from {table} where account_name = {account}").format(
+                table = sql.Identifier(provider),
+                account = sql.Literal(account)
+            )
+        
+        cur.execute(query)
+        if cur.fetchone() is not None:
+            print(f'{account} already exists in {provider}, use the update command to change credentials')
+            return
 
         # prompt user for each argument we'll need for cost
-        vals = map(lambda a: input("{}: ".format(a)) if a == 'account_name' else getpass("{}: ".format(a)), cols)
+        vals = list(map(lambda a: getpass("{}: ".format(a)),
+                   filter(lambda a: a != 'account_name',cols))) # Skip prompting account_name
+        # Push our account_name onto the top
+        vals.insert(0, account)
 
         # Build arbritary insert using the psycopg2 sql extension
         # Need to convert everything into Identifier and Literal for this to work so map cols and vals
@@ -186,6 +222,96 @@ def add_account(cur, provider):
             print(f"Unexpected {err=}, {type(err)=}")
             raise
 
+# Update an account
+def update_account(cur, **kwargs):
+    args = kwargs['args']
+    provider = args.iaas
+    account = args.account
+
+    # First check if provider module exists
+    # It doesn't we can't do anything
+    try:
+        module = importlib.import_module("providers.{}".format(provider))
+
+        # Get argument names from module's run command
+        argspec = inspect.getfullargspec(module.cost)
+        # argspec[0] is a list of names of standard arguments
+        cols = argspec[0]
+        
+        # Make sure account exists
+        query = sql.SQL("select top 1 from {table} where account_name = {account}").format(
+                table = sql.Identifier(provider),
+                account = sql.Literal(account)
+            )
+        
+        cur.execute(query)
+        if cur.fetchone() is None:
+            print(f'{account} does not exist in {provider}, use the add add command')
+            return
+        
+        # prompt user for each argument we'll need for cost
+        vals = list(map(lambda a: getpass("{}: ".format(a)),
+                   filter(lambda a: a != 'account_name',cols))) # Skip prompting account_name
+        # Push our account_name onto the top
+        vals.insert(0, account)
+
+        # Build arbritary replace using the psycopg2 sql extension
+        # Need to convert everything into Identifier and Literal for this to work so map cols and vals
+        replace = sql.SQL("replace into {table}({columns}) VALUES ({values})").format(
+                table = sql.Identifier(provider),
+                columns = sql.SQL(',').join(map(lambda a: sql.Identifier(a), cols)),
+                values = sql.SQL(',').join(map(lambda a: sql.Literal(a), vals)))
+
+        # Execute query and commit change
+        cur.execute(replace)
+        cur.connection.commit()
+
+    # For now die here, need to find what exceptions can be thrown here
+    except BaseException as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+            raise
+
+# List either providers available or accounts (optionally in provider)
+def list_account(cur, **kwargs):
+    args = kwargs['args']
+    # List all providers implemented
+    if args.type == 'providers':
+        # Get import spec for our providers package
+        spec = util.find_spec('providers')
+        # Grab its full path
+        pathname = Path(spec.origin).parent
+        # Scan entire directory for files ending with .py
+        # And exclude anything starting with __
+        with os.scandir(pathname) as entries:
+            for entry in entries:
+                if entry.name.startswith('__'):
+                    continue
+                current = entry.name.partition('.')[0]
+                if entry.is_file():
+                    if entry.name.endswith('.py'):
+                        print(current)
+    # List of all accounts
+    # Takes an optional --iaas argument, list of providers to list from
+    elif args.type == 'accounts':
+        # Grab a list of all tables in the DB (should be a list of provider names)
+        # this is postgresql specific
+        cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+
+        # fetchall() will return us a dictionary of lists
+        # we only want the first value of each list
+        tables = cur.fetchall()
+        providers = map(lambda a: a[0], tables)
+        # Filter by ones in our passed list (if we have one)
+        if args.iaas is not None:
+            providers = filter(lambda a: a in args.iaas, providers)
+        
+        # Just loop through every row in every table as filtered above and print account_name
+        for provider in providers:
+            cur.execute("SELECT * FROM {}".format(provider))
+            rows = cur.fetchall()
+            
+            for row in rows:
+                print(f"{provider}: {row['account_name']}")
 
 def main(args):
     # Connect to our postgres database
@@ -205,26 +331,11 @@ def main(args):
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
             
             # Fork here depending on if we're running cost or adding a provider
-            if args.add is not None:
-                add_account(cur, args.add)
-            else:
-                fname = run_cost(cur)
-                # Retry upload 5 times
-                for i in range(5):
-                    try:
-                        upload_file(fname, **conf['mattermost'])
-                    except Exception as err:
-                        # Failed but we have retries left
-                        if i < 4:
-                            # Print and continue
-                            print(err)
-                            continue
-                        else:
-                            # Retries exceeded, pass the exception up
-                            raise
-                    else:
-                        # Success, break from retry loop
-                        break
+            # Handle this by simpling calling the set function by argparse
+            # We can pass anything the functions may need since they're declared
+            # as kwargs
+            args.func(cur, args=args, conf=conf)
+                
 
     # Did we screw up connecting to database?
     except (Exception, psycopg2.DatabaseError) as error:
@@ -241,9 +352,29 @@ def main(args):
 
 # Python standard, do nothing if imported
 if __name__ == '__main__':
-    # Check for arguments (these only exist if you want to add accounts)
+    # Parse our arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--add", type=str, help="--add PROVIDER Add an account for PROVIDER to the database")
+    parser.set_defaults(func=run_cost)
+    subparsers = parser.add_subparsers(help='sub-commands, type <command> --help to get more information')
+    
+    sub_list = subparsers.add_parser('list', help='list accounts or providers')
+    sub_list.add_argument('type', choices=['accounts', 'providers'], help='the item to list')
+    sub_list.add_argument('--iaas', action='extend', nargs='+', type=str, required=False, help='optional list of providers to list accounts from')
+    sub_list.set_defaults(func=list_account)
+    
+    sub_update = subparsers.add_parser('update', help='update an exist account\'s credentials')
+    sub_update.add_argument('--iaas', type=str, required=True, help='iaas to modify account in')
+    sub_update.add_argument('--account', type=str, required=True, help='account to modify')
+    sub_update.set_defaults(func=update_account)
+    
+    sub_add = subparsers.add_parser('add', help='add a new account')
+    sub_add.add_argument('--iaas', type=str, required=True, help='iaas to add account to')
+    sub_add.add_argument('--account', type=str, required=True, help='account name to add')
+    sub_add.set_defaults(func=add_account)
+    
+    sub_remove = subparsers.add_parser('remove', help='remove an account')
+    sub_remove.add_argument('--iaas', type=str, required=True, help='iaas to remove account from')
+    sub_remove.add_argument('--account', type=str, required=True, help='account name to remove')
 
     args = parser.parse_args()
 
